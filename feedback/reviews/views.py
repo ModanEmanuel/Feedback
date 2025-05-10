286ce951b9d30493e44edf178b55f5ff3a581e7c
ModanEmanuel <modan.emy@gmail.com>
2025-05-10 10:45:58 +0300

    Added a get_context_data method in the SigleReviewView class setting up the session

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -50,6 +50,15 @@ class SingleReviewView(DetailView):
     template_name = 'reviews/single_review.html'
     model = Review
 
+    def get_context_data(self, **kwargs): # This method defines the context that will be set for the template
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object # This special "object" property in this class will give us access to the automaticaly loaded review
+        request = self.request # This special "request" property in this class will give us access to the request
+        favorite_id = request.session.get('favorite_review') # This is the currently stored favorite ID
+        context['is_favorite'] = favorite_id == str(loaded_review.id) # This compares if the loaded review ID is the same as the favorite review's ID
+        # By default the loaded_review is a int, but we need it to be a string
+        return context
+
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
@@ -57,3 +66,12 @@ class SingleReviewView(DetailView):
     #     selected_review = Review.objects.get(pk=review_id)
     #     context['review'] = selected_review
     #     return context
+
+class AddFavoriteView(View):
+    def post(self, requests):
+        review_id = requests.POST['review_id'] # Getting the review id
+        requests.session['favorite_review'] = review_id # Storing the review id in a session
+        return HttpResponseRedirect('/reviews/' + review_id) # Redirecting
+
+# OBS: Data stored in a session must contain simple data(strings, numbers, booleans, dictionaries)
+# DO NOT STORE OBJECTS ! Like functions or classes
